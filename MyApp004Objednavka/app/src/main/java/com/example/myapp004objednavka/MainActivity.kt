d5bb7d664b94aaf19320aab92ef6b2fe28ec1de7
Tomáš Žižka <tomas.zizka@tul.cz>
2024-10-03 14:32:49 +0200

    #4 - Objednavka - MainActivity.kt - zakomentovani

--- MyApp004Objednavka/app/src/main/java/com/example/myapp004objednavka/MainActivity.kt
+++ MyApp004Objednavka/app/src/main/java/com/example/myapp004objednavka/MainActivity.kt
@@ -9,12 +9,14 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        // enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
+        }*/
     }
 }
