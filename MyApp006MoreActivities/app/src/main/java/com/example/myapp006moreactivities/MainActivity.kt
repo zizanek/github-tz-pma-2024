40470a7ad05da0e97f990cd876c0bf4618afecca
tomas.zizka <tomas.zizka@tul.cz>
2024-10-10 15:03:20 +0200

    #6 MoreActivities - logika pro prvni aktivitu

--- MyApp006MoreActivities/app/src/main/java/com/example/myapp006moreactivities/MainActivity.kt
+++ MyApp006MoreActivities/app/src/main/java/com/example/myapp006moreactivities/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.myapp006moreactivities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +12,22 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        //enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnSecond = findViewById<Button>(R.id.btnSecond)
+        val etNickname = findViewById<EditText>(R.id.etNickname)
+
+        btnSecond.setOnClickListener {
+            val nickname = etNickname.text.toString() // získáme text z edit text pole
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("NICK_NAME", nickname)
+            startActivity(intent)
         }
+
+
+        
     }
 }
