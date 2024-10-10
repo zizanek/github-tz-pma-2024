3c0334485a140a1f36255b72110b47f8ca65e975
tomas.zizka <tomas.zizka@tul.cz>
2024-10-10 11:18:16 +0200

    #5 Jetpack Compose App - Scaffold komponenta

--- MyApp005JetpackCompose/app/src/main/java/com/example/myapp005jetpackcompose/MainActivity.kt
+++ MyApp005JetpackCompose/app/src/main/java/com/example/myapp005jetpackcompose/MainActivity.kt
@@ -9,8 +9,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapp005jetpackcompose.ui.theme.MyApp005JetpackComposeTheme
 
@@ -29,6 +32,21 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ComposePerson() {
 
+    // Přidáme Scaffold, abychom mohli přidat TopAppBar
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Moje Aplikace", color = Color.White) }, // Nastaví barvu textu na bílou
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = Color.DarkGray,  // Nastaví pozadí na černé
+                    //titleContentColor = Color.White // Nastaví barvu textu na bílou
+                )
+            )
+        }
+    ) {
+
+
+    }
 
 
 }
