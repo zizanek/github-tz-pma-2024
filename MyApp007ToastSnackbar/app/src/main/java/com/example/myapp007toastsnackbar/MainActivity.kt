10de2b4ea468c201af723f1e8914a43e42300ca4
Tomáš Žižka <tomas.zizka@tul.cz>
2024-10-17 11:52:41 +0200

    #7 MyApp007 - Snackbar a jeho nastavení

--- MyApp007ToastSnackbar/app/src/main/java/com/example/myapp007toastsnackbar/MainActivity.kt
+++ MyApp007ToastSnackbar/app/src/main/java/com/example/myapp007toastsnackbar/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.myapp007toastsnackbar
 
 import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.accessibility.AccessibilityEventCompat.setAction
 import com.example.myapp007toastsnackbar.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,16 +23,30 @@ class MainActivity : AppCompatActivity() {
             binding = ActivityMainBinding.inflate(layoutInflater)
             setContentView(binding.root)
 
-            // Nastavení akce pro tlačítko Toast
+            // Nastavení akce pro tlačítko Zobrazit TOAST
             binding.btnShowToast.setOnClickListener {
 
-                val toast = Toast.makeText(this, "Nazdar - mám hlad", Toast.LENGTH_LONG)
+                val toast = Toast.makeText(this, "Nazdar - mám hlad", Toast.LENGTH_SHORT)
 
                 toast.show()
             }
 
+            // Nastavení akce pro tlačítko Zobrazit SNACKBAR
 
+            binding.btnShowSnackbar.setOnClickListener {
 
+                Snackbar.make(binding.root, "Já jsem SNACKBAR",Snackbar.LENGTH_SHORT)
 
+                    .setDuration(7000)
+                    .setBackgroundTint(Color.parseColor("#FF35AA"))
+                    .setTextColor(Color.BLACK)
+                    .setActionTextColor(Color.WHITE)
+
+                    .setAction("Zavřít") {
+                        Toast.makeText(this, "Zavírám SNACKBAR", Toast.LENGTH_SHORT).show()
+                    }
+
+                    .show()
+            }
     }
 }
