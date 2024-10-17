f9ffe68876a61f7cb9961af8d63ac712fbbdf256
Tomáš Žižka <tomas.zizka@tul.cz>
2024-10-17 11:12:54 +0200

    #7 MyApp007 - základ pro binding

--- MyApp007ToastSnackbar/app/src/main/java/com/example/myapp007toastsnackbar/MainActivity.kt
+++ MyApp007ToastSnackbar/app/src/main/java/com/example/myapp007toastsnackbar/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.example.myapp007toastsnackbar
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapp007toastsnackbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+            // Inicializace ViewBinding
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+
+
     }
 }
